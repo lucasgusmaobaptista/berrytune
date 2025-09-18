e09548f4896d4da08d120169fb3c555e9d115f78
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-18 15:46:01 -0300

    first commit

---
+++ src/main/java/me/lucasgusmao/berrytuneapi/BerrytuneapiApplication.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.berrytuneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerrytuneapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerrytuneapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lucasgusmao/berrytuneapi/BerrytuneapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lucasgusmao.berrytuneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerrytuneapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
