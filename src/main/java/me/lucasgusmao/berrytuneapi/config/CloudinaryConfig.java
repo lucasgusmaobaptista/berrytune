423aef545e82ef4a13c227bb7b4db3d178830065
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-18 17:27:56 -0300

    Consertando erros na conexão com Cloudinary

--- src/main/java/me/lucasgusmao/berrytuneapi/config/CloudinaryConfig.java
+++ src/main/java/me/lucasgusmao/berrytuneapi/config/CloudinaryConfig.java
@@ -1,7 +1,6 @@
 package me.lucasgusmao.berrytuneapi.config;
 
 import com.cloudinary.Cloudinary;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,21 +9,12 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
-    private String cloudName;
-
-    @Value("${cloudinary.api-key}")
-    private String apiKey;
-
-    @Value("${cloudinary.api-secret}")
-    private String apiSecret;
-
     @Bean
     public Cloudinary cloudinary() {
         Map<String, String> config = Map.of(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret);
+                "cloud_name", "dktstsdl1",
+                "api_key", "627281215297798",
+                "api_secret", "QhxFu01wUaAv79i5IEKXwESIVno");
 
         return new Cloudinary(config);
     }
