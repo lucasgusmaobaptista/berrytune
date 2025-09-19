29474016af448732207e3c89c36a27dd7de44ca9
Lucas <lucasgusmaobaptista@gmail.com>
2025-09-19 13:11:56 -0300

    Adicionada lógica para deletar e listar albums

--- src/main/java/me/lucasgusmao/berrytuneapi/repository/AlbumRepository.java
+++ src/main/java/me/lucasgusmao/berrytuneapi/repository/AlbumRepository.java
@@ -4,6 +4,8 @@ import me.lucasgusmao.berrytuneapi.model.Album;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AlbumRepository extends MongoRepository<Album, String> {
 }
